cacac92323dcc923304af20b782fbc131ecca4fb
Chris Lomuntad <chris@goautodial.com>
2014-03-17 09:38:50 -0400

    modified models/golist.php for the bug when downloading leads

--- application/models/golist.php
+++ application/models/golist.php
@@ -99,9 +99,10 @@ class Golist extends Model {
 		    {
 			 $valid_custom_table=1;
 		    }
+
+                    $added_custom_SQL  = ", $custom_table ct";
+                    $added_custom_SQL2 = "AND vl.lead_id=ct.lead_id";
 	       }
-	       $added_custom_SQL  = ", $custom_table ct";
-	       $added_custom_SQL2 = "AND vl.lead_id=ct.lead_id";
 	  }
 
 	  $stmt = "SELECT vl.lead_id AS lead_id,entry_date,modify_date,status,user,vendor_lead_code,source_id,list_id,gmt_offset_now,called_since_last_reset,phone_code,phone_number,title,first_name,middle_initial,last_name,address1,address2,address3,city,state,province,postal_code,country_code,gender,date_of_birth,alt_phone,email,security_phrase,comments,called_count,last_local_call_time,rank,owner{$header_columns} FROM vicidial_list vl{$added_custom_SQL} WHERE list_id='$listid' $added_custom_SQL2;";
